0915de2dad5b59534f8921a9e8490a6915197531
Marek Fijalkowski <gitmaro1983@gmail.com>
2022-09-25 18:50:34 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Programmer("Anna", "Kowalska", "Java");
+        System.out.println(employee.toString());
+    }
+}
